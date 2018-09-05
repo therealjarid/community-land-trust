514cfa953bd664fe11b7e8b251421a2cde683e63
jaridwarren <jaridwarren@gmail.com>
2018-09-05 15:15:23 -0700

    add favicon and login logo

--- themes/community-land-trust/inc/extras.php
+++ themes/community-land-trust/inc/extras.php
@@ -20,3 +20,37 @@ function clt_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'clt_body_classes' );
+
+function clt_login_logo()
+{
+    echo '<style type="text/css">                                                                   
+	body.login div#login h1 a { 
+		background-image:url('.get_stylesheet_directory_uri().'/assets/images/logos/logo-horizontal.svg) !important; 
+		background-size: 320px 70px !important;
+		width: 320px;
+		margin: 0;
+	} 
+	#loginform {
+		margin-top: 0;
+	}
+	</style>';
+}
+add_action('login_head', 'clt_login_logo');
+
+/**
+ * The following changes the logo url 
+ */
+
+function clt_login_url( $url )
+{
+    return get_bloginfo(home_url());
+}
+add_filter('login_headerurl', 'clt_login_url');
+
+/**
+ * Add Favicon to backend of WordPress.
+ */
+function show_favicon() {
+	echo '<link href="'.get_stylesheet_directory_uri().'/assets/images/favicon.ico" rel="icon" type="image/x-icon">';
+}
+add_action('admin_head', 'show_favicon');
