25b80b3259872633fac1ab1f466c60dee12248bf
jaridwarren <jaridwarren@gmail.com>
2018-09-05 16:19:37 -0700

    start header format and style

--- themes/community-land-trust/header.php
+++ themes/community-land-trust/header.php
@@ -24,11 +24,13 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<a href="<?php echo home_url(); ?>">
+                		<div class="header-logo"></div>
+            		</a>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+					<i class="fas fa-bars"></i>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
