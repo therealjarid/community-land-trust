43d90c2e149d9fec81bf3efad86af1a2ea440874
elim322 <einer.lim322@gmail.com>
2018-09-06 16:33:51 -0700

    format footer

--- themes/community-land-trust/footer.php
+++ themes/community-land-trust/footer.php
@@ -7,22 +7,22 @@
 
 ?>
 
-			</div><!-- #content -->
-			<footer id="colophon" class="site-footer container" role="contentinfo">
-				<div class="main-footer">
-				<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("footer") ) : ?>
-				<?php endif;?>
-				</div>
-				<div class="sub-footer">
-				<div class="copyright">
-				<?php $current_year = date("Y");
-				echo ('Copyright ' . '&copy; ' . $current_year . ' ' . 'Community Land Trust' );?>
-				</div>
-				</div>
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+</div><!-- #content -->
+<footer id="colophon" class="site-footer container" role="contentinfo">
+    <div class="main-footer">
+		<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( "footer" ) ) : ?>
+		<?php endif; ?>
+    </div>
+    <div class="sub-footer">
+        <div class="copyright">
+			<?php $current_year = date( "Y" );
+			echo( 'Copyright ' . '&copy; ' . $current_year . ' ' . 'Community Land Trust' ); ?>
+        </div>
+    </div>
+</footer><!-- #colophon -->
+</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 
-	</body>
+</body>
 </html>
