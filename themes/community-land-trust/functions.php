a8c5ac4b769a858d6c78fd2783be3467e4fb82e8
jaridwarren <jaridwarren@gmail.com>
2018-09-07 10:28:15 -0700

    remove local fonts, add Google Font, add highlighting to header

--- themes/community-land-trust/functions.php
+++ themes/community-land-trust/functions.php
@@ -102,7 +102,10 @@ function clt_scripts() {
 	
 	//adding flickity styles via CDN
 	wp_enqueue_style( 'clt-flickity', 'https://unpkg.com/flickity@2/dist/flickity.min.css');
-	
+
+	//adding Google Fonts
+	wp_enqueue_style('clt-google-fonts', 'https://fonts.googleapis.com/css?family=Raleway:400,700|Roboto:400,700');
+
 	// add font awesome via CDN 
 	wp_enqueue_style( 'clt-font-awesome-icons', 'https://use.fontawesome.com/releases/v5.2.0/css/all.css');
 
