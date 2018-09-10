128f53e2b5e0a6c05ef8c39c31954f723aff088d
elim322 <einer.lim322@gmail.com>
2018-09-10 11:30:27 -0700

    edit contact.php

--- themes/community-land-trust/page-templates/contact.php
+++ themes/community-land-trust/page-templates/contact.php
@@ -6,3 +6,19 @@
  */
 
 get_header(); ?>
+<section class="contact-us">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</section>
+
+
+<?php get_footer(); ?>
