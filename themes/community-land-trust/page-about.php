1cde3ace123eb642e849d6edd6d7aa906d2b0324
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-09-10 21:34:31 -0700

    small adjustments with faq page styles

--- themes/community-land-trust/page-about.php
+++ themes/community-land-trust/page-about.php
@@ -1,2 +1,27 @@
-<?php get_header(); ?>
+
+<?php
+/**
+ * The template for displaying about page.
+ * Template Name: About
+ * 
+ * @package CLT_Theme
+ */
+
+get_header(); ?>
+
+<section class="contact-us">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</section>
+
+
 <?php get_footer(); ?>
--- themes/community-land-trust/src/js/faq-page.js
+++ themes/community-land-trust/src/js/faq-page.js
@@ -11,6 +11,6 @@ jQuery(document).ready(function($) {
       }
 
   });
-
+  
 
 });
