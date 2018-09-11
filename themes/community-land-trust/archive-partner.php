f61cc670f2cad326b251c003e40e32899a315e4e
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-09-11 14:47:44 -0700

    attemp to pull correct content on archive-partner.php

--- themes/community-land-trust/archive-partner.php
+++ themes/community-land-trust/archive-partner.php
@@ -7,5 +7,30 @@
  */
 
 get_header(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+
+<?php the_post_thumbnail(); ?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+          <img class="logo" src="<?php echo CFS()->get( 'about_page_clt_logo' ); ?>"></img>
+
+          <div class="entry-content">
+      <?php the_content(); ?>
+      <?php
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+        'after'  => '</div>',
+      ) );
+      ?>
+          </div><!-- .entry-content -->
+      </article><!-- #post-## -->
+
+<?php endwhile; // End of the loop. ?>
+
+<span>Connect with us to learn how! Contact CLT’s <?php echo CFS()->get( 'partners_contact_position' ); ?></span>
+<span>, <?php echo CFS()->get( 'partners_contact_name' ); ?><span>
+<span>, at <?php echo CFS()->get( 'partners_contact_email' ); ?></span>
+
 
 <?php get_footer(); ?>
