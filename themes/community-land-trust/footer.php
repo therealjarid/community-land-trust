036b04075ba420d57c44001e4638d93da38a292d
elim322 <einer.lim322@gmail.com>
2018-09-12 10:56:22 -0700

    adding cfs

--- themes/community-land-trust/footer.php
+++ themes/community-land-trust/footer.php
@@ -13,22 +13,24 @@
     <div class="contact-info">
         <p class="contact-number">
         <i class="fas fa-phone fa-rotate-180"></i>
-	    <?php echo CFS()->get( 'contact_number' ); ?>
+	    <?php echo CFS()->get( 'contact_number', 36 ); ?>
         </p>
         <p class="email-address">
         <i class="fas fa-envelope"></i>
-		<?php echo CFS()->get( 'email' ); ?>
+		<?php echo CFS()->get( 'email', 36 ); ?>
         </p>
-    </div>
-    <div class="address">
-    <a href="https://www.google.ca/maps?q=1651+commercial+drive&rlz=1C5CHFA_enJP803JP803&um=1&ie=UTF-8&sa=X&ved=0ahUKEwiB2ca5h7TdAhWprlQKHZVOC6QQ_AUICigB"
-    target="_blank"><img class="footer-image" src="<?php echo CFS()->get( 'footer_map_image' );?>"/>
-    <p><?php echo CFS()->get( 'location' );?></p>
     </div>
     <div class="social-media">
     <?php echo CFS()->get( 'facebook' );?>
     <?php echo CFS()->get( 'twitter' );?>
     <?php echo CFS()->get( 'linkedin' );?>
+    </div>
+
+    <div class="address">
+    <img class="footer-image" <?php echo CFS()->get( 'footer_map_image' );?>>
+    <p><i class="fas fa-map-marker-alt"></i><?php echo CFS()->get( 'location' );?></p>
+
+    </div>
 
 
 
