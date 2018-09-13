ff67f15256a4801951ee0694db0d2aa23d3ded1d
elim322 <einer.lim322@gmail.com>
2018-09-12 21:08:44 -0700

    format files

--- themes/community-land-trust/404.php
+++ themes/community-land-trust/404.php
@@ -9,27 +9,27 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<div class="container">
-					<div class="text-wrap">
-				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( '404' ); ?></h1>
-				</header><!-- .page-header -->
+        <section class="error-404 not-found">
+            <div class="container">
+                <div class="text-wrap">
+                    <header class="page-header">
+                        <h1 class="page-title"><?php echo esc_html( '404' ); ?></h1>
+                    </header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php echo esc_html( "We're not sure what you're looking for, but we'll get you home safely!" ); ?></p>
-				</div><!-- .page-content -->
-				<a href="<?php echo get_home_url(); ?>">
-            <button class="mixin-button-404">Back Home</button>
-				</a>
-				</div>
-				</div><!-- .container -->
-			</section><!-- .error-404 -->
+                    <div class="page-content">
+                        <p><?php echo esc_html( "We're not sure what you're looking for, but we'll get you home safely!" ); ?></p>
+                    </div><!-- .page-content -->
+                    <a href="<?php echo get_home_url(); ?>">
+                        <button class="mixin-button-404">Back Home</button>
+                    </a>
+                </div>
+            </div><!-- .container -->
+        </section><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
