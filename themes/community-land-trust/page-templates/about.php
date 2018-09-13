6488a8d3c245293000a58b5c57cac2695637faa4
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-09-12 22:03:18 -0700

    amend php code on about.php and start mobile styles

--- themes/community-land-trust/page-templates/about.php
+++ themes/community-land-trust/page-templates/about.php
@@ -10,24 +10,28 @@ get_header(); ?>
 
 <section class="about-us">
 
-	<?php the_post_thumbnail(); ?>
+	<div class="about-header">
+			<?php the_post_thumbnail(); ?>
+	</div>
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-            <img class="logo" src="<?php echo CFS()->get( 'about_page_clt_logo' ); ?>"></img>
+						<div class="about-clt-logo">
+								<img src="<?php echo CFS()->get( 'about_page_clt_logo' ); ?>"></img>
+						</div>
 
-            <div class="entry-content">
-				<?php the_content(); ?>
-				<?php
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-					'after'  => '</div>',
-				) );
-				?>
-            </div><!-- .entry-content -->
-        </article><!-- #post-## -->
+        		<div class="entry-content">
+							<?php the_content(); ?>
+							<?php
+							wp_link_pages( array(
+								'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+								'after'  => '</div>',
+							) );
+							?>
+        	</div><!-- .entry-content -->
+      	</article><!-- #post-## -->
 
 	<?php endwhile; // End of the loop. ?>
 
@@ -35,24 +39,36 @@ get_header(); ?>
 
 		<?php $about_us_fields = CFS()->get( 'about_our_history_highlights' ); ?>
 
-		
+			<?php foreach ( $about_us_fields as $about_us_field ) : ?>
 
-			<?php foreach ( $about_us_fields as $about_us_field ) {
-				echo "<h3 class='about-cfs-years'>" . $about_us_field['about_page_history_year'] . "</h3>";
-				echo "<p class='about-cfs-description'>" . $about_us_field['about_page_history_description'] . "</p>";
-				echo "<p class='about-cfs-extra-description'>" . $about_us_field['about_page_history_extra_description'] . "</p>";
-				}
-			?>
+					<h3 class='about-cfs-years'> 
+						<?php echo $about_us_field['about_page_history_year']; ?> 
+					</h3>
+
+					<p class='about-cfs-description'>
+						<?php echo $about_us_field['about_page_history_description']; ?>
+					</p>
+
+					<p class='about-cfs-extra-description'> 
+						<?php echo $about_us_field['about_page_history_extra_description']; ?>
+					</p>
+					
+			<?php
+      	endforeach;
+      ?>
 			
 		</div>
 
-	
-
 		<h2 class="more-info">Need More Information?</h2>
 
-		<a href="<?php echo get_permalink( get_page_by_path( 'contact' ) ) . "#contact-container"; ?>">
-			<button class="mixin-button">Contact Us</button>
-		</a>
+		<div class="button-container">
+			<button class="mixin-button">
+				<a href="<?php echo get_permalink( get_page_by_path( 'contact' ) ) . "#contact-container"; ?>">
+						Contact Us
+				</a>
+			</button>
+		</div>
+		
 
 </section><!-- .about-us -->
 
