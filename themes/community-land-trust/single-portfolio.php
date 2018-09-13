6a7301ab70f767356077653414c9200c1940e453
jaridwarren <jaridwarren@gmail.com>
2018-09-13 09:55:35 -0700

    add more-info mixin, start single-portfolio work

--- themes/community-land-trust/single-portfolio.php
+++ themes/community-land-trust/single-portfolio.php
@@ -9,59 +9,71 @@
 get_header(); ?>
 
 	<div class="single-portfolio-container">
-
-		<?php while ( have_posts() ) : the_post(); ?>
-
-          
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-        <div class="hero-container">
-          <?php if ( has_post_thumbnail() ) : ?>
-            <?php the_post_thumbnail(); ?>
-          <?php endif; ?>
-
-          <?php the_title( '<h1 class="portfolio-title">', '</h1>' ); ?>
-
-        </header><!-- .entry-header -->
-
-        <div class="entry-content">
-          <?php the_content(); ?>
-          <?php
-            wp_link_pages( array(
-              'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-              'after'  => '</div>',
-            ) );
-            ?>
+           
+    <div class="hero-container">
+      <?php if ( has_post_thumbnail() ) : ?>
+      <?php the_post_thumbnail(); ?>
+      <?php endif; ?>
       
-            
-            <div class="main-carousel">
+      <div class="title-container">
+        <?php the_title( '<h1 class="portfolio-title">', '</h1>' ); ?>
+      </div>
+      
+    </div>
+    
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-            <?php
-            $portfolio_gallery = CFS()->get( 'portfolio_gallery' );
+      <div class="entry-content">
 
-              foreach ( $portfolio_gallery as $gallery_image ) {
-                
-                $image = $gallery_image['portfolio_gallery_image'];
-                  echo "<div class='carousel-cell'>
-                        <img src='{$image}'>
-                        </div>";
-                }
+        <?php the_content(); ?>
 
-              ?>
+      </div><!-- .entry-content -->
 
-            </div> 
+      <div class="fields-container">
 
+          <!-- insert custom fields here -->
+
+      </div>
+
+
+      <?php
+        wp_link_pages( array(
+          'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+          'after'  => '</div>',
+        ) );
+        ?>   
+        
+        <div class="main-carousel">
+
+          <?php
+          $portfolio_gallery = CFS()->get( 'portfolio_gallery' );
+
+            foreach ( $portfolio_gallery as $gallery_image ) {
               
+              $image = $gallery_image['portfolio_gallery_image'];
+                echo "<div class='carousel-cell'>
+                      <img src='{$image}'>
+                      </div>";
+              }
 
-        </div><!-- .entry-content -->
-      </article><!-- #post-## -->
+          ?>  
+        </div>   
 
-			<?php the_post_navigation(); ?>
+        <?php the_post_navigation(); ?>
 
+    </article><!-- #post-## -->
 
-		<?php endwhile; // End of the loop. ?>
+    <div class="more-info">
+
+      <h2>Need More Info<span class="contraction">rmation</span>?</h2>
+
+      <button class="cta-button">
+          <a href="<?php echo get_permalink( get_page_by_title( 'Contact Us' ) ) . "#contact-container" ?>">Contact
+        Us</a>
+      </button>
+
+    </div>
 
 	</div><!-- #primary -->
-
 
 <?php get_footer(); ?>
