e24dfb3d37a8fc8c6612948ccedecdc23ee4a8dd
elim322 <einer.lim322@gmail.com>
2018-09-13 12:57:55 -0700

    edit mobile view for single media

--- themes/community-land-trust/single.php
+++ themes/community-land-trust/single.php
@@ -9,13 +9,16 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 
-
+		<div class="media-container">
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <header class="entry-header">
 			<?php if ( has_post_thumbnail() ) : ?>
 				<?php the_post_thumbnail( 'large' ); ?>
 			<?php endif; ?>
+			<div class="title-date">
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<?php clt_posted_on(); ?>
+			</div>
         </header><!-- .entry-header -->
 
         <div class="single-content-container">
@@ -28,15 +31,15 @@ get_header(); ?>
 				'after'  => '</div>',
 			) );
 			?>
-        </div><!-- end partner-container -->
+        </div><!-- end single-content-container -->
 		</article><!-- #post-## -->
 		
 		<div class="navigation-links">
-			<button><?php previous_post_link( '%link', '&larr;' ); ?></button>
-			<button><a href="<?php echo get_permalink( get_page_by_title( 'Portfolio' ) ) ?>">Portfolio</a></button>
-			<button><?php next_post_link( '%link', '&rarr;' ); ?></button>
+			<?php previous_post_link( '%link', '&larr;' ); ?>
+			<a href="<?php echo get_permalink( get_page_by_title( 'Portfolio' ) ) ?>">Portfolio</a>
+			<?php next_post_link( '%link', '&rarr;' ); ?>
 		</div>
-
+		</div> <!-- end of media-container --> 
 
 </div><!-- #primary -->
 
