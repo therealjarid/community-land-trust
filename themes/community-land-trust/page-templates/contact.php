64bea3fbde1e31a31bad2afb1be1374359aca747
jaridwarren <jaridwarren@gmail.com>
2018-09-15 14:20:36 -0700

    remove unneeded divs, pull in gravity form from function call instead of shorthand

--- themes/community-land-trust/page-templates/contact.php
+++ themes/community-land-trust/page-templates/contact.php
@@ -8,53 +8,35 @@
 get_header(); ?>
 
 <div class="contact-us">
-    <div id="primary" class="content-area">
-
-
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="entry-header">
-            <h1 class="entry-title"><?php the_title(); ?></h1>
-        </div><!-- .entry-header -->
         
-            <section class="upper-content">
-                <div class="contact-info">
-                    <p class="contact-number">
-                        <i class="fas fa-phone fa-rotate-180"></i>
-						<?php echo CFS()->get( 'contact_number' ); ?>
-                    </p>
-                    <p class="email-address">
-                        <i class="fas fa-envelope"></i>
-						<?php echo CFS()->get( 'email' ); ?>
-                    </p>
-                    <p class="address">
-                        <i class="fas fa-map-marker-alt"></i>
-						<?php echo CFS()->get( 'location' ); ?>
-                    </p>
-                </div>
-                <div class="map-container">
-                    <div id="map-canvas"></div>
-                </div>
-            </section>
-        </article><!-- #post-## -->
-
-    </div>
+    <div class="entry-header">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+    </div><!-- .entry-header -->
+    
+    <section class="upper-content">
+    <!-- @TODO: these should all pull in dynamic hyperlinks for each  -->
+        <div class="contact-info">
+            <p class="contact-number">
+                <i class="fas fa-phone fa-rotate-180"></i>
+                <?php echo CFS()->get( 'contact_number' ); ?>
+            </p>
+            <p class="email-address">
+                <i class="fas fa-envelope"></i>
+                <?php echo CFS()->get( 'email' ); ?>
+            </p>
+            <p class="address">
+                <i class="fas fa-map-marker-alt"></i>
+                <?php echo CFS()->get( 'location' ); ?>
+            </p>
+        </div>
+        <div class="map-container">
+            <div id="map-canvas"></div>
+        </div>
+    </section>
 
     <div class="contact-container">
         <a id="contact-container"></a>
-
-		<?php while ( have_posts() ) :
-			the_post(); ?>
-
-			<?php the_content(); ?>
-			<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-			?>
-
-		<?php endwhile; // End of the loop. ?>
-
+		<?php gravity_form( 1, true, true, false, null, false, null, true ); ?>
     </div><!-- .contact-container -->
 
     <div class="featured-person">
@@ -62,6 +44,7 @@ get_header(); ?>
             <img src="<?php echo CFS()->get( 'photo' ); ?>"/>
         </div>
         <div class="featured-person-content">
+        <!-- @TODO - mailto: link should be pulled dynamically -->
             <a href="mailto:info@cltrust.ca">
                 <h2 class="featured-person-name"><?php echo CFS()->get( 'name' ); ?></h2>
             </a>
