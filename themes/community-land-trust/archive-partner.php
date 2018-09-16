b73d149a80c92eeb8defc7246b627f99cdfa08ce
jaridwarren <jaridwarren@gmail.com>
2018-09-15 22:23:37 -0700

    styles for partner archive for both desktop and large desktop

--- themes/community-land-trust/archive-partner.php
+++ themes/community-land-trust/archive-partner.php
@@ -6,55 +6,59 @@
  * @package CLT_Theme
  */
 
-get_header(); 
+get_header(); ?>
+    <div class="partners-page-container">
 
-    while ( have_posts() ) : the_post(); ?>
-    
-      <div class="partner-page-content">
+        <?php while ( have_posts() ) : the_post(); ?>
+        
+          <div class="partner-page-content">
 
-          <?php the_content(); ?>
+              <?php the_content(); ?>
 
-      </div><!-- .partner-page-content -->
+          </div><!-- .partner-page-content -->
 
-    <?php endwhile; ?>
-    
-    <div class="main-carousel">
+        <?php endwhile; ?>
+        
+        <div class="flickity-container">
+            <div class="main-carousel">
 
-      <?php
+              <?php
 
-        $query = new WP_Query( array( 'post_type' => 'Partner',
-                            ) );      
+                $query = new WP_Query( array( 'post_type' => 'Partner',
+                                    ) );      
 
-        if ( $query->have_posts() ) :
-          while ( $query->have_posts() ) : $query->the_post();?>
+                if ( $query->have_posts() ) :
+                  while ( $query->have_posts() ) : $query->the_post();?>
 
-            <div class="carousel-cell">
-              <div class="partner-info">
-                    <?php the_post_thumbnail();
-                      the_content();?> 
-              </div>     
-              <div class="partner-meta">
-                <p><?php echo esc_html( CFS()->get( 'partner_info', false ) ); ?></p>
-                <p><?php echo esc_html( CFS()->get( 'partner_completion', false ) ); ?></p>
-              </div>
+                    <div class="carousel-cell">
+                      <div class="partner-info">
+                            <?php the_post_thumbnail();
+                              the_content();?> 
+                      </div>     
+                      <div class="partner-meta">
+                        <p><?php echo esc_html( CFS()->get( 'partner_info', false ) ); ?></p>
+                        <p><?php echo esc_html( CFS()->get( 'partner_completion', false ) ); ?></p>
+                      </div>
 
+                    </div>
+                  <?php endwhile;
+                endif ?>
+                
             </div>
-          <?php endwhile;
-        endif ?>
+        </div>
 
-    </div>
+        <div class="contact-container">
 
-    <div class="contact-container">
+          <p><?php echo esc_html( CFS()->get( 'contact_copy', 175 ) ); ?></p>
+          <p><?php 
+            // 175 is the post id for the Partners page
+            echo 'Connect with us to learn how! Contact CLT’s ' . esc_html( CFS()->get( 'partners_contact_position', 175 ) ) . ', ';
+            echo esc_html( CFS()->get( 'partners_contact_name', 175 ) ) . ', at ';
+            echo '<a href="mailto:' . esc_html( CFS()->get( 'partners_contact_email', 175 ) ) . '" >';
+            echo esc_html( CFS()->get( 'partners_contact_email', 175 ) );
+            echo '</a></p></div>';
+          ?>
 
-      <p><?php echo esc_html( CFS()->get( 'contact_copy', 175 ) ); ?></p>
-      <p><?php 
-        // 175 is the post id for the Partners page
-        echo 'Connect with us to learn how! Contact CLT’s ' . esc_html( CFS()->get( 'partners_contact_position', 175 ) ) . ', ';
-        echo esc_html( CFS()->get( 'partners_contact_name', 175 ) ) . ', at ';
-        echo '<a href="mailto:' . esc_html( CFS()->get( 'partners_contact_email', 175 ) ) . '" >';
-        echo esc_html( CFS()->get( 'partners_contact_email', 175 ) );
-        echo '</a></p></div>';
-      ?>
-
+  </div>
 
 <?php get_footer(); ?>
