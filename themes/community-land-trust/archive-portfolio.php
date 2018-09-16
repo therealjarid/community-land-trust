d4d9ed3a699ea5d1139adffa884c21341f410ba1
jaridwarren <jaridwarren@gmail.com>
2018-09-15 23:00:19 -0700

    add branding banner to portfolio page

--- themes/community-land-trust/archive-portfolio.php
+++ themes/community-land-trust/archive-portfolio.php
@@ -12,33 +12,36 @@ get_header(); ?>
 	) );
 	?>
 
-    <div class="button-container">
+    <div class="banner-container">
+        <div class="button-container">
 
-        <button id="0" class="selected">All</button>
+            <button id="0" class="selected">All</button>
 
-		<?php foreach ( $terms as $term ) {
-			echo '<button id="' . $term->term_id . '" >' . $term->name . '</button>';
-		} ?>
+            <?php foreach ( $terms as $term ) {
+                echo '<button id="' . $term->term_id . '" >' . $term->name . '</button>';
+            } ?>
 
+        </div>    
+        <p class="error-message">Please refresh the page and try again.</p>
     </div>
 
-    <p class="error-message">Please refresh the page and try again.</p>
+    <div class="offset-container">
+        <div class="portfolio-grid-container">
 
-    <div class="portfolio-grid-container">
+            <?php // js fills in containers here ?>
 
-		<?php // fill in containers here ?>
+        </div>
 
-    </div>
+        <div class="more-info">
 
-    <div class="more-info">
+            <h2>Need More Info<span class="contraction">rmation</span>?</h2>
 
-        <h2>Need More Info<span class="contraction">rmation</span>?</h2>
+            <button class="cta-button">
+                <a href="<?php echo esc_url(get_permalink( get_page_by_title( 'Contact Us' ) )) . "#contact-container" ?>">Contact
+                    Us</a>
+            </button>
 
-        <button class="cta-button">
-            <a href="<?php echo esc_url(get_permalink( get_page_by_title( 'Contact Us' ) )) . "#contact-container" ?>">Contact
-                Us</a>
-        </button>
-
+        </div>
     </div>
 
 </div>
