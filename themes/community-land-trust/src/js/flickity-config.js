303ff22f7a49bcefc82f9c1cf55c8fd90d4d639a
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-09-18 14:31:01 -0700

    changed flickity arrows

--- themes/community-land-trust/src/js/flickity-config.js
+++ themes/community-land-trust/src/js/flickity-config.js
@@ -2,6 +2,14 @@ jQuery(document).ready(function($) {
   $('.main-carousel').flickity({
     cellAlign: 'left',
     contain: true,
-    wrapAround: true
+    wrapAround: true,
+
+ 
+    arrowShape: { 
+    x0: 10,
+    x1: 60, y1: 50,
+    x2: 60, y2: 50,
+    x3: 60
+    }
   });
 });
