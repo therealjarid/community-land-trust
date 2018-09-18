9b79dea41272cc6c6502349be47e589e870b5751
jaridwarren <jaridwarren@gmail.com>
2018-09-18 15:26:10 -0700

    add OG meta data

--- themes/community-land-trust/functions.php
+++ themes/community-land-trust/functions.php
@@ -298,3 +298,32 @@ function clt_remove_wp_archives(){
 }
 add_action('template_redirect', 'clt_remove_wp_archives');
 
+// add the OG data in the Language Attributes
+function add_opengraph_doctype( $output ) {
+	return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+}
+add_filter('language_attributes', 'add_opengraph_doctype');
+
+// add OG meta info
+
+function insert_fb_in_head() {
+	global $post;
+	if ( !is_singular() ) // if it is not a post or a page
+		return;
+		echo '<meta property="fb:admins" content="https://www.facebook.com/pages/Community-Land-Trust/144302426226897"/>';
+		echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+		echo '<meta property="og:type" content="article"/>';
+		echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+		echo '<meta property="og:site_name" content="Community Land Trust"/>';
+	if ( !has_post_thumbnail( $post->ID ) ) { // the post does not have featured image, use a default image
+		$default_image = get_template_directory_uri() . "/assets/images/logos/logo-main.png";
+		echo '<meta property="og:image" content="' . $default_image . '"/>';
+	}
+	else {
+		$thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+		echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+	}
+	echo "";
+}
+add_action( 'wp_head', 'insert_fb_in_head', 5 );
+
