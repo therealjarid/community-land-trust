d4403283b8f9f5f1aa248819ca8fb6fcb436ba6e
elim322 <einer.lim322@gmail.com>
2018-09-18 09:50:16 -0700

    add background

--- themes/community-land-trust/page-templates/contact.php
+++ themes/community-land-trust/page-templates/contact.php
@@ -16,7 +16,7 @@
         <h2 class="entry-title"><?php the_title(); ?></h2>
     </div><!-- .entry-header -->
   
-    <section class="contact-email-address">
+    <section class="upper-content">
         <div class="contact-info">
             <p class="contact-number">
                 <i class="fas fa-phone fa-rotate-180"></i>
@@ -36,7 +36,6 @@
         </div>
     </section>
     </div> <!-- end of banner container -->
-
     <div class="offset-container">
     <div class="contact-container">
         <a id="contact-container"></a>
