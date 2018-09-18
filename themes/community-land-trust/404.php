557686736ca5f9f73b5fc9e045f7459e96b782b0
jaridwarren <jaridwarren@gmail.com>
2018-09-18 11:53:43 -0700

    fix 404 CTA

--- themes/community-land-trust/404.php
+++ themes/community-land-trust/404.php
@@ -23,7 +23,7 @@ get_header(); ?>
                         <p><?php echo esc_html( "We're not sure what you're looking for, but we'll get you home safely!" ); ?></p>
                     </div><!-- .page-content -->
                     <a href="<?php echo esc_url(get_permalink( get_page_by_title( 'Find a Home' ) )); ?>">
-                        <button class="mixin-button-404">Back Home</button>
+                        <button class="mixin-button-404">Find a Home</button>
                     </a>
                 </div>
             </div><!-- .container -->
